5a16a1ce3496b23b8e2c8feb700404a7cfb4c5ca
xdTobs <70943428+xdTobs@users.noreply.github.com>
2022-11-01 15:09:26 +0100

    initial commit

---
+++ src/main/java/Language.java
@@ -0,0 +1,8 @@
+public class Language {
+    public final String[] fieldNames = {"","Tower","Crater","Palace Gates","Cold Desert","Walled City","Monastery","Black Cave","Huts in the mountain","The Werewall","The Pit","Goldmine"};
+    public final String gameWon = "has won the game";
+    public final String playerName1 = "Player1";
+    public final String playerName2 = "Player2";
+    public final String onRollDice = "It is your turn. Roll the Dice";
+    public final String onWereWall = "Landed on the WereWall. You get an extra turn";
+}
