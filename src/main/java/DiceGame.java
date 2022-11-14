551715637ed851a3faae50e906a9cb458c83d5f7
YesSeri <henrik.zenkert@gmail.com>
2022-11-15 00:46:08 +0100

    Fixed bug fix bug. Gameover check all players.

--- src/main/java/DiceGame.java
+++ src/main/java/DiceGame.java
@@ -1,3 +1,4 @@
+import Dice.DiceCup;
 import gui_fields.*;
 import gui_main.GUI;
 
@@ -10,13 +11,13 @@ public class DiceGame {
     Player[] players = new Player[playerCount];
     GUI gui;
 
-    boolean[] jailedPlayers =new boolean[playerCount];
+    boolean[] jailedPlayers = new boolean[playerCount];
     Language language = new Language();
     GUI_Controller gui_controller;
     int[] playerPos = new int[playerCount];
     int playerTurn;
     Chance[] cards;
-    final int[] fieldValues = {1,1,1,1,2,2,2,2,3,3,3,3,4,4,5,5};
+    final int[] fieldValues = {1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 5, 5};
 
     GameBoard gameBoard = new GameBoard(language);
 
@@ -28,11 +29,11 @@ public class DiceGame {
         car1.setPrimaryColor(Color.black);
         GUI_Car car2 = new GUI_Car();
         car2.setPrimaryColor(Color.white);
-        this.players[0] = new Player(language.playerName1, 20,car1);
-        this.players[1] = new Player(language.playerName2, 20,car2);
+        this.players[0] = new Player(language.playerName1, 20, car1);
+        this.players[1] = new Player(language.playerName2, 20, car2);
 
         this.gui = new GUI(gameBoard.getFields(), Color.white);
-        this.gui_controller = new GUI_Controller(gui,players, gameBoard.getFields());
+        this.gui_controller = new GUI_Controller(gui, players, gameBoard.getFields());
         gui_controller.addPlayersToGUI();
         this.cards = initializeChanceCards();
     }
@@ -40,14 +41,15 @@ public class DiceGame {
     private void playGame() {
         playerTurn = 0;
 
-        for (Player player : players){
-            movePlayer(0,player);
+        for (Player player : players) {
+            movePlayer(0, player);
         }
         while (true) {
             Player currentPlayer = players[playerTurn];
             playRound(currentPlayer);
             if (isGameover()) {
                 //needs to be changed to count money from other players.
+                // Should be a scoreboard showing first to last according to amount of money
                 this.gui.showMessage(currentPlayer.getName() + language.gameWon);
                 break;
             }
@@ -62,45 +64,42 @@ public class DiceGame {
         int j = 0;
         for (int i = 0; i < fields.length; i++) {
 //GUI_Street(" ", " ", " ", " ", Color.BLACK, Color.BLACK);
-                switch (i) {
-                    case (0):
-                        fields[i] = new GUI_Start("Start", " ", " ", Color.white, Color.black);
-                        break;
-                    case(3):
-                    case(9):
-                    case(15):
-                    case(21):
+            switch (i) {
+                case (0):
+                    fields[i] = new GUI_Start("Start", " ", " ", Color.white, Color.black);
+                    break;
+                case (3):
+                case (9):
+                case (15):
+                case (21):
 
-                        fields[i] = new GUI_Chance();
-                        break;
-                    case(6):
-                        //implement proper constructor
-                        fields[i] = new GUI_Jail();
-                        break;
-                    case(12):
-                        //implement proper constructor
-                        fields[i] = new GUI_Refuge();
-                        break;
-                    case(18):
-                        //implement go to jail
-                        fields[i] = new GUI_Refuge();
-                        break;
-                    default:
-                        fields[i] = streets[j];
-                        j++;
-                        break;
-                }
-
-
+                    fields[i] = new GUI_Chance();
+                    break;
+                case (6):
+                    //implement proper constructor
+                    fields[i] = new GUI_Jail();
+                    break;
+                case (12):
+                    //implement proper constructor
+                    fields[i] = new GUI_Refuge();
+                    break;
+                case (18):
+                    //implement go to jail
+                    fields[i] = new GUI_Refuge();
+                    break;
+                default:
+                    fields[i] = streets[j];
+                    j++;
+                    break;
+            }
 
 
         }
 
 
-
-
         return fields;
     }
+
     private GUI_Street[] initializeStreets() {
         GUI_Street[] streets;
         streets = new GUI_Street[16];
@@ -111,46 +110,48 @@ public class DiceGame {
         }
         return streets;
     }
-    private Chance[] initializeChanceCards(){
+
+    private Chance[] initializeChanceCards() {
         List<Chance> cards = new ArrayList<>();
         cards.add(new moveChance(
-                language.moveTo+" "+gameBoard.getFields()[3].getTitle(),
+                language.moveTo + " " + gameBoard.getFields()[3].getTitle(),
                 gui_controller,
                 gameBoard.getFields()[2])
         );
         Chance[] temp = new Chance[cards.size()];
-return cards.toArray(new Chance[0]);
+        return cards.toArray(new Chance[0]);
     }
-    private void checkFieldType(Player currentPlayer){
-        GUI_Field field = currentPlayer.getCar().getPosition();
-        if (field instanceof GUI_Street street){
-            onStreet(street,currentPlayer);
 
-        }
-        else if (field instanceof GUI_Chance) onChance(cards[0],currentPlayer);
-        else if(field instanceof GUI_Jail) return ;
-        else if (field instanceof GUI_Start) return ;
-        else if (field instanceof GUI_Refuge) return ;
-        else return ;
+    private void checkFieldType(Player currentPlayer) {
+        GUI_Field field = currentPlayer.getCar().getPosition();
+        if (field instanceof GUI_Street street) {
+            onStreet(street, currentPlayer);
+
+        } else if (field instanceof GUI_Chance) onChance(cards[0], currentPlayer);
+        else if (field instanceof GUI_Jail) return;
+        else if (field instanceof GUI_Start) return;
+        else if (field instanceof GUI_Refuge) return;
+        else return;
     }
-    private void onStreet (GUI_Street street,Player currentPlayer){
+
+    private void onStreet(GUI_Street street, Player currentPlayer) {
         //maybe prompt that you landed
-        int rent =Integer.parseInt(street.getRent());
-        if (street.getOwnerName().equals("Bank")){
-                //currently just shows player id, would be nice to have full names for GUI clicks
-                street.setOwnerName(String.valueOf(playerTurn));
-                currentPlayer.setBalance(currentPlayer.getBalance()-rent,gui_controller);
+        int rent = Integer.parseInt(street.getRent());
+        if (street.getOwnerName().equals("Bank")) {
+            //currently just shows player id, would be nice to have full names for GUI clicks
+            street.setOwnerName(String.valueOf(playerTurn));
+            currentPlayer.setBalance(currentPlayer.getBalance() - rent, gui_controller);
 
 
             //dont think we need an else, since we still want to purchace and then gameover if the player cant afford
-        }
-        else{
-            players[Integer.parseInt(street.getOwnerName())].setBalance(players[Integer.parseInt(street.getOwnerName())].getBalance()+rent,gui_controller);
-            currentPlayer.setBalance(currentPlayer.getBalance()-rent,gui_controller);
+        } else {
+            players[Integer.parseInt(street.getOwnerName())].setBalance(players[Integer.parseInt(street.getOwnerName())].getBalance() + rent, gui_controller);
+            currentPlayer.setBalance(currentPlayer.getBalance() - rent, gui_controller);
         }
     }
-    private void onChance(Chance chance,Player currentPlayer){
-        gui_controller.displayText(currentPlayer.getName()+" "+language.onChanceCard+chance.getDesc());
+
+    private void onChance(Chance chance, Player currentPlayer) {
+        gui_controller.displayText(currentPlayer.getName() + " " + language.onChanceCard + chance.getDesc());
         if (chance instanceof moveChance moveChance) moveChance.pullCard(currentPlayer);
 
     }
@@ -161,20 +162,20 @@ return cards.toArray(new Chance[0]);
         diceCup.roll();
         int diceSum = diceCup.getSum();
         //might need to change language class to a txt file instead, make sure to check with helpers
-        if (currentPlayer.isJailed()){
-            currentPlayer.setBalance(currentPlayer.getBalance()-1,gui_controller);
+        if (currentPlayer.isJailed()) {
+            currentPlayer.setBalance(currentPlayer.getBalance() - 1, gui_controller);
             currentPlayer.setJailed(false);
         }
         gui.showMessage(currentPlayer.getName() + " " + language.onRollDice);
 
         gui.setDie(diceSum);
         //make sure player doesnt move out of bounds
-        int nextPos =currentPlayer.getPos()+diceSum;
-        if (nextPos>=24)currentPlayer.setPos(nextPos-24);
+        int nextPos = currentPlayer.getPos() + diceSum;
+        if (nextPos >= 24) currentPlayer.setPos(nextPos - 24);
         else currentPlayer.setPos(nextPos);
 
         //move player
-        movePlayer(currentPlayer.getPos(),currentPlayer);
+        movePlayer(currentPlayer.getPos(), currentPlayer);
 
         //onfield handeling, maybe could be put in another class
         checkFieldType(currentPlayer);
@@ -188,8 +189,6 @@ return cards.toArray(new Chance[0]);
     }
 
 
-
-
     private void nextPlayer() {
         if (playerTurn >= players.length - 1) {
             playerTurn = 0;
@@ -198,13 +197,15 @@ return cards.toArray(new Chance[0]);
         }
     }
 
+    // The only player that can lose money is the current player, I think. Maybe the chance cards can take money from the player, but I don't think so.
+    // Never mind. There is a chance card that takes money from all other players and gives to current player. We need to check all players.
     private boolean isGameover() {
-        int winLimit = 0;
-        boolean res = false;
-        for (Player player : players){
-            if (player.getBalance()<0) res = true;
+        for (Player player : players) {
+            if (player.getBalance() <= 0) {
+                return true;
+            }
         }
-        return res;
+        return false;
     }
 
     public static void main(String[] args) {
