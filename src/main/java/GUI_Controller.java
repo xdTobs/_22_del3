385f2b1a6d24cbed85896ed665356046db9147d3
YesSeri <henrik.zenkert@gmail.com>
2022-11-15 13:16:56 +0100

    More robust language handling with easier translations. Still missing chance cards.

--- src/main/java/GUI_Controller.java
+++ src/main/java/GUI_Controller.java
@@ -6,44 +6,39 @@ import java.util.SplittableRandom;
 
 public class GUI_Controller {
     GUI gui;
-Player[] players;
+    Player[] players;
     GUI_Player[] GUI_Players;
     GUI_Field[] fields;
 
-    public GUI_Controller(GUI gui, Player[] players, GUI_Field[] fields){
+    public GUI_Controller(GUI gui, Player[] players, GUI_Field[] fields) {
         this.gui = gui;
         this.fields = fields;
         this.players = players;
         GUI_Players = new GUI_Player[players.length];
         for (int i = 0; i < players.length; i++) {
-            GUI_Players[i] = new GUI_Player(players[i].getName(),players[i].getBalance(),players[i].getCar());
+            GUI_Players[i] = new GUI_Player(players[i].getName(), players[i].getBalance(), players[i].getCar());
         }
 
     }
 
-    public void addPlayersToGUI(){
+    public void addPlayersToGUI() {
         for (GUI_Player player :
                 GUI_Players) {
             gui.addPlayer(player);
+        }
     }
-    }
+
     public void movePlayer(int pos, int playerId) {
-
         GUI_Players[playerId].getCar().setPosition(fields[pos]);
-
     }
 
-    public void updatePlayer(int id){
-    Player p =players[id];
-    GUI_Player gp = GUI_Players[id];
-    gp.setBalance(p.getBalance());
+    public void updatePlayer(int id) {
+        Player p = players[id];
+        GUI_Player gp = GUI_Players[id];
+        gp.setBalance(p.getBalance());
     }
 
-    public void displayText(String text){
+    public void displayText(String text) {
         gui.showMessage(text);
     }
-
-
-
-
 }
