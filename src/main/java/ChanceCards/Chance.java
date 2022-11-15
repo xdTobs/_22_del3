3ca188fb1bb47490c80bdfee9e9ec05c341107ba
YesSeri <henrik.zenkert@gmail.com>
2022-11-15 20:26:36 +0100

    Made Chance pullCard method abstract

--- src/main/java/ChanceCards/Chance.java
+++ src/main/java/ChanceCards/Chance.java
@@ -2,15 +2,13 @@ package ChanceCards;
 
 import Enities.Player;
 import Controllers.*;
-import Language.LanguageHandler;
 
 public abstract class Chance {
 
     protected String desc;
     protected GUI_Controller gui_controller;
-    protected LanguageHandler l = LanguageHandler.getInstance();
-    public void pullCard(Player p){};
 
+    public abstract void pullCard(Player p);
 
     public String getDesc() {
         return desc;
