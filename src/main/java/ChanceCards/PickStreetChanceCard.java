ef54d5a99a0b2d0b9edf61ab8244ba283f7f25de
YesSeri <henrik.zenkert@gmail.com>
2022-11-19 20:02:47 +0100

    Forgot one file

--- src/main/java/ChanceCards/PickStreetChanceCard.java
+++ src/main/java/ChanceCards/PickStreetChanceCard.java
@@ -1,73 +1,47 @@
 package ChanceCards;
 
-import Enities.Fields.Field;
-import Enities.Fields.Street;
+import Enities.GameBoard;
 import Language.LanguageHandler;
-import gui_fields.GUI_Street;
-import Enities.*;
-import View.*;
-import gui_main.GUI;
+import View.GUI_View;
 
 /**
- * Chance card to pick a street to move to. Also buys / plays rent when landed on.
+ * Chance card to pick a street to move to.
+ * Get the street for free or pay rent when landed on.
+ * We create two seperate GoToStreetCards in here and let the player select the one they prefer.
  */
 public class PickStreetChanceCard extends ChanceCard {
-    private int[] choices;
-    private String[] options;
-    private int index;
-    private Street street;
+    private GotoStreetAndBuyCard[] gotoStreetCards;
+    private GotoStreetAndBuyCard selectedCard = null;
 
-
-    public PickStreetChanceCard(int[] choices) {
-
-
-        this.desc = "temp";
-        this.choices = choices;
-
-
+    public PickStreetChanceCard(GotoStreetAndBuyCard[] gotoStreetAndBuyCards) {
+        if (gotoStreetAndBuyCards.length != 2) {
+            throw new IllegalArgumentException("There should be 2 gotoStreetAndBuyCards.");
+        }
+        this.gotoStreetCards = gotoStreetAndBuyCards;
     }
 
-    public void executeCardAction(Player[] players, Player currentPlayer) {
-
-
-        currentPlayer.setPosition(street.getPosition());
-        if (street.getOwner().equals("Bank")) {
-            currentPlayer.addBalance(-street.getRent());
-            street.setOwner(currentPlayer.getName());
-        } else {
-//            gameHandler.gameBoard.onStreet(street, currentPlayer, gui_controller, gameHandler.players);
-//            street.getOwner().addBalance(street.getRent());
-            Player owner = null;
-            for (Player p : players) {
-                if (p.getName().equals(street.getOwner())) {
-                    owner = p;
-                }
-            }
-            owner.addBalance(street.getRent());
-            currentPlayer.addBalance(-street.getRent());
-        }
-    }
-
-
-
-
-    public int chooseStreet(GUI gui, Player p, Field[] fields){
-        options = new String[choices.length];
-        for (int i = 0; i < choices.length; i++) {
-            options[i] = (fields[choices[i]]).getName();
-        }
-        String s = gui.getUserSelection(p.getName()+" "+LanguageHandler.chanceCardMsg() + " " + LanguageHandler.onPickFieldChance(), options);
-        for (int i = 0; i < options.length; i++) {
-            if (fields[choices[i]].getName().equals(s)) {
-                return choices[i];
-
+    public void promptPlayerForStreet(GUI_View view) {
+        String message = LanguageHandler.chanceCardMsg() + " " + LanguageHandler.onPickFieldChance();
+        String[] choices = getStreetChoiceNames();
+        String answer = view.promptPlayer(choices, message);
+        for (GotoStreetAndBuyCard gotoStreetCard :
+                gotoStreetCards) {
+            if (gotoStreetCard.getStreetName().equals(answer)) {
+                selectedCard = gotoStreetCard;
             }
         }
-
-return -1;
     }
 
-    public void setStreet(Street street) {
-        this.street = street;
+    public void executeCardAction(GameBoard gameBoard) {
+        selectedCard.executeCardAction(gameBoard);
+        selectedCard = null;
+    }
+
+    public String[] getStreetChoiceNames() {
+        String[] choices = new String[gotoStreetCards.length];
+        for (int i = 0; i < gotoStreetCards.length; i++) {
+            choices[i] = gotoStreetCards[i].getStreetName();
+        }
+        return choices;
     }
 }
