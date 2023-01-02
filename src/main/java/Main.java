a05478a4ce1c86a5980e76cdd624609359315c54
xdTobs <70943428+xdTobs@users.noreply.github.com>
2023-01-02 14:25:59 +0100

    removed useless reference to LanguageHandler

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,7 +11,6 @@ public class Main {
      * The entry point of application.
      */
     public static void main(String[] args) throws IOException {
-        LanguageHandler l = new LanguageHandler();
         GameBoard gameBoard = new GameBoard();
         GUI_View view = new GUI_View(gameBoard.getFields());
 
