0c9f01623a8f810ed73a600bf291254b605faca6
YesSeri <henrik.zenkert@gmail.com>
2023-01-10 19:54:24 +0100

    Fixed small thing with test

--- src/test/java/entities/FieldImplTest.java
+++ src/test/java/entities/FieldImplTest.java
@@ -1,18 +1,16 @@
 package entities;
 
-import controller.BasicUserIO;
-import controller.GameController;
-import controller.UserIO;
-import controller.View;
+import controller.*;
+import entities.chancecards.Deck;
 import entities.dicecup.DiceCup;
 import entities.dicecup.TestDie;
-import entities.fields.Field;
-import entities.fields.Street;
-import language.LanguageController;
+import entities.fields.*;
 import language.Message;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import view.GuiView;
 import view.TestView;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,43 +18,38 @@ class FieldImplTest {
 
     @Test
     void buyHouseProcess() {
-        
-    }
-    @Test
-  void gotoJail() {
+        DiceCup dc = new DiceCup(new TestDie[]{new TestDie(1), new TestDie(2)});
+        Field[] fields = GameBoard.getDefaultFields();
+        BasicUserIO userIO = new BasicUserIO() {
+            @Override
+            public int promptChoice(Message message, Message... choices) {
+                //override the choice to always want to buy a house on the first avaliable street
+                if (message.getType().equals(Message.selectHouse().getType()))
+                    return 1;
+                return 0;
+            }
 
-      DiceCup dc = new DiceCup(new TestDie[]{new TestDie(1), new TestDie(2)});
-      Field[] fields = GameBoard.getDefaultFields();
-      BasicUserIO userIO = new BasicUserIO() {
-          @Override
-          public int promptChoice(Message message, Message... choices) {
-              //override the choice to always want to buy a house on the first avaliable street
-              if (message.getType().equals(Message.selectHouse().getType()))
-                  return 1;
-              return 0;
-          }
+            @Override
+            public int promptRange(Message message, int min, int max) {
+                throw new RuntimeException("shouldnt be here in this test");
+            }
 
-          @Override
-          public int promptRange(Message message, int min, int max) {
-              throw new RuntimeException("shouldnt be here in this test");
-          }
+            @Override
+            public void showMessage(Message message) {
 
-          @Override
-          public void showMessage(Message message) {
+            }
+        };
+        UserIO testUserIO = new UserIO(userIO);
+        GameBoard gameBoard = new GameBoard(dc, fields, testUserIO, 2);
+        View view = new TestView();
 
-          }
-    };
-      UserIO testUserIO = new UserIO(userIO);
-        GameBoard gameBoard = new GameBoard(dc,fields,testUserIO,2);
-      View view = new TestView();
+        GameController gameController = new GameController(view, testUserIO, gameBoard);
+        gameController.resetPlayerPositions();
 
-      GameController gameController = new GameController(view, testUserIO,gameBoard);
-      gameController.resetPlayerPositions();
-
-      //Hardcode giving the first pair of streets to player 1
-        Street street1 =(Street)gameBoard.getField(1);
+        //Hardcode giving the first pair of streets to player 1
+        Street street1 = (Street) gameBoard.getField(1);
         street1.setOwner(gameBoard.getCurrentPlayer());
-        Street street2 =(Street)gameBoard.getField(3);
+        Street street2 = (Street) gameBoard.getField(3);
         street2.setOwner(gameBoard.getCurrentPlayer());
         //also add this to the ownership map, since they are not bought through normal process
         gameBoard.getOwnershipMap().get(gameBoard.getCurrentPlayer()).add(street1);
@@ -68,11 +61,33 @@ class FieldImplTest {
             gameBoard.nextPlayer();
         }
         //player2 balance should be 24000 since they pay rent with hotel for 6000
-        assertEquals(24000,gameBoard.getPlayers()[1].getBalance());
+        assertEquals(24000, gameBoard.getPlayers()[1].getBalance());
         //player1 balance should be 26000 since they buy houses for 10000 and recieve 6000 rent
-        assertEquals(26000,gameBoard.getPlayers()[0].getBalance());
+        assertEquals(26000, gameBoard.getPlayers()[0].getBalance());
+
+    }
+
+    @Test
+    @DisplayName("If player lands on go to jail, he should get automatically moved to jail and his status should be set to jailed")
+    void gotoJail() {
+        Field[] fields = new Field[3];
+
+        fields[0] = FieldTest.getStartFieldDebug();
+        fields[1] = new GoToJail("Ga i fængsel,1, jail,,,,,,,,");
+        fields[2] = new Jail("I fængsel/På besøg,2, jail,,,,,,,,");
+
+        DiceCup diceCup = new DiceCup(new TestDie[]{new TestDie(1), new TestDie(0)});
+        TestUserIO testUserIO = TestUserIO.debugSetup();
+        Deck deck = new Deck(new ArrayList<>());
+        GameBoard gameBoard = new GameBoard(diceCup, fields, deck, testUserIO, 2);
+        GameController gameController = new GameController(new TestView(), testUserIO, gameBoard);
+
+        // Player should go one step, land on go to jail and then get moved to jail.
+        gameController.playTurn(gameBoard.getCurrentPlayer());
 
 
-  }
-    
+        assertEquals(2, gameBoard.getCurrentPlayer().getPosition());
+        assertTrue(gameBoard.getCurrentPlayer().isJailed());
+    }
+
 }
